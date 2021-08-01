43a7e87cc0107ec04c51467c356ff9b94c14e1a9
Kazi Rayhan <rayhanbd4400@gmail.com>
2021-08-01 10:47:22 +0600

    first commit

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Gallery = () => {
+const Gallery = (props: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined) => {
     return (
         <div>
-            
+            {props}
         </div>
     );
 };
